cc06f4ad00a9377d7c20cf005b14c6c82e4b095e
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-07-23 02:40:53 +0500

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,3 @@
 import * as firebase from "firebase";
 
-var config = {
-    apiKey: "AIzaSyBpRx6xmkuu9CuQS8qCV2ITMs9v9ll27RQ",
-    authDomain: "task-monitor101.firebaseapp.com",
-    databaseURL: "https://task-monitor101.firebaseio.com",
-    projectId: "task-monitor101",
-    storageBucket: "task-monitor101.appspot.com",
-    messagingSenderId: "809895032505"
-};
-
-firebase.initializeApp(config);
-
-
 export const databaseRef = firebase.database().ref('/tasks'); 
