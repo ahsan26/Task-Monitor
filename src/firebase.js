6fc6ee24464b1d2c4fcc83931d884534636945e8
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-09-20 01:20:57 +0500

    fixed the month number issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import * as firebase from "firebase";
 
-var config = {
-    apiKey: "AIzaSyBpRx6xmkuu9CuQS8qCV2ITMs9v9ll27RQ",
-    authDomain: "task-monitor101.firebaseapp.com",
-    databaseURL: "https://task-monitor101.firebaseio.com",
-    projectId: "task-monitor101",
-    storageBucket: "task-monitor101.appspot.com",
-    messagingSenderId: "809895032505"
-  };
-  firebase.initializeApp(config);
+// Write your firebase credentials here
 
 export const databaseRef = firebase.database().ref('/tasks'); 
